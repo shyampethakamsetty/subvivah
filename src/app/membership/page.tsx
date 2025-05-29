e2102a55f7b3106a39b4296c27f68547ed9196da
siva shyam <sivashyam@gmail.com>
2025-05-29 20:39:09 +0530

    deleted

--- src/app/membership/page.tsx
+++
@@ -1,9 +0,0 @@
-import Membership from '@/components/Membership';
-
-export default function MembershipPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Membership />
-    </div>
-  );
-} 
